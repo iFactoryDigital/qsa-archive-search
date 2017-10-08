bce02d4fee1e245eb15da8b117d09448bee3c633
Hiro Nozu <hiro@ifactory.com.au>
2017-10-09 09:31:53 +1000

    Including the deployment script.

---
+++ merge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#--exclude "dist/app.qsa.search.bundle.js.map" \
+rsync --recursive --verbose --progress \
+--exclude ".git" \
+--exclude ".gitignore.ifactory" \
+--exclude "node_modules" \
+--exclude "dist/index.html" \
+--exclude "src/public/index.html" \
+--exclude "dist/.gitignore" \
+--exclude "dist/products.csv" \
+--exclude "dist/products_old.csv" \
+--exclude "dist/dsiti/qsa/search/categories.json" \
+--exclude "src/public/.gitignore" \
+--exclude "src/public/products_old.csv" \
+--exclude "merge.sh" \
+--exclude "update_permission_dev.sh" \
+. ../qsa-archive-search/
+
+cd ../qsa-archive-search-commit
+git add --all
